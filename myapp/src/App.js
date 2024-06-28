3941f115e82518606d565bc19b751dad3ce9b42f
sneha404 <snehajaisinghani3005@gmail.com>
2024-06-28 09:34:38 +0530

    Initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,346 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <div className="container-fluid sec1">
+        <div className="row r1">
+          <div className="col-lg-6 c1">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <span>info@example.com</span>
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <span>123 456-7890</span>
+          </div>
+          <div className="col-lg-6">
+            <div className="c3">
+              <div className="icons">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </div>
+              <div className="icons">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </div>
+              <div className="icons">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </div>
+              <div className="icons">
+                <i class="fa fa-pinterest" aria-hidden="true"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row r2">
+          <div className="col-lg-6 c4">
+            Prixima<span>.</span>
+          </div>
+          <div className="col-lg-6 c5">
+            <button>HOME</button>
+            <button>ABOUT</button>
+            <button>Services</button>
+            <button>Portfolio</button>
+            <button>Team</button>
+            <button>Reviews</button>
+            <button>Blog</button>
+            <button>Contact</button>
+
+          </div>
+        </div>
+
+        <div className="row r3">
+          <div className="img">
+            <button className='prev'>PREV</button>
+            <div className='text1'>WE CRAFT DIGITAL EXPERIANCES</div>
+            <div className="text2">DESIGN DRIVEN FOR PROFESSIONALS</div>
+            <button className='gs'>GET STARTED</button>
+            <button className='ow'>OUR WORK</button>
+            <button className='next'>NEXT</button>
+          </div>
+        </div>
+
+        <div className=" row r4">
+
+          <div className="col-lg-6">
+            <div className='myflex'>
+              <div className="icons1  ic1">
+                <i class="fa fa-search  " aria-hidden="true"></i>
+              </div>
+              <div>
+                <div className="dm">
+                  Digital Marketing
+                </div>
+                <div className="txt1">
+                  It is a long established fact that a reader will be
+                  distracted by the readable content of a page
+                </div>
+              </div>
+            </div>
+
+            <div className='myflex'>
+              <div className="icons1 ic2">
+                <i class="fa fa-camera " aria-hidden="true"></i>
+              </div>
+              <div>
+                <div className="dm">
+                  E-mail Marketing
+                </div>
+                <div className="txt1">
+                  It is a long established fact that a reader will be
+                  distracted by the readable content of a page
+                </div>
+              </div>
+            </div>
+
+            <div className='myflex'>
+              <div className="icons1 ic3">
+                <i class="fa fa-microphone " aria-hidden="true"></i>
+              </div>
+              <div>
+                <div className="dm">
+                  Business Marketing
+                </div>
+                <div className="txt1">
+                  It is a long established fact that a reader will be
+                  distracted by the readable content of a page
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-lg-6 ">
+            <div className="photo"></div>
+          </div>
+
+        </div>
+
+        <div className="r5">
+          <div className="row">
+            <div className="col-lg">
+              <div className="numbers">
+                90K +
+              </div>
+              <div className="txt3">
+                Happy Clients
+              </div>
+            </div>
+            <div className="col-lg">
+              <div className="numbers">
+                45 M
+              </div>
+              <div className="txt3">
+                Lines of code
+              </div>
+            </div>
+            <div className="col-lg">
+              <div className="numbers">
+                190
+              </div>
+              <div className="txt3">
+                Total Downloads
+              </div>
+            </div>
+            <div className="col-lg">
+              <div className="numbers">
+                380K
+              </div>
+              <div className="txt3">
+                Youtube Subscribers
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r6">
+          <div className="col-lg-12 txt4">
+            OUR SERVICES
+          </div>
+          <div className="col-lg-12 txt5">
+            WHAT WE DO ?
+          </div>
+          <div className="col-lg-12 txt6">
+            Contrary to popular belief. lorem ipsum is not a simply random text.It has
+            roots in a piece of classical latin literature from 45 BC, making it over
+            2000 years old
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-address-card" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Digital Marketing
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-android" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Logo Designing
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-amazon" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Business consulting
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-camera-retro" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Videography
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-microchip" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Brand Identity
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box1">
+              <div className="icon1">
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </div>
+              <div className="heading">
+                Ethical Hacking
+              </div>
+              <div className="text3">
+                Contrary to popular belief. lorem ipsum is not a simply random text.It has
+                roots in a piece of classical latin literature from 45 BC, making it over
+                2000 years old
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r7">
+          <div className="col-lg-12">
+            <div className="txt1">
+              WORK
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div className="txt2">
+              Successful projects
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div className="txt3">
+              Contrary to popular belief. lorem ipsum is not a simply random text.It has
+              roots in a piece of classical latin literature from 45 BC, making it over
+              2000 years old
+            </div>
+          </div>
+          <div className="col-lg-3 cc">
+            <div className="img1 ">
+              <div className="imgcc">
+                <div className="consult">
+                  <div>Consulting Marketing</div>
+
+                  <div className='wd'><div className='line'></div>  Website Design</div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+          <div className="col-lg-6 cc">
+            <div className="img2">
+              <div className="imgcc">
+                <div className="consult">
+                  <div>Consulting Marketing</div>
+                  <div className='wd'><div className='line'></div>  Website Design</div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className="col-lg-3 cc">
+            <div className="img3 ">
+              <div className="imgcc">
+                <div className="consult">
+                  <div>Consulting Marketing</div>
+
+                  <div className='wd'><div className='line'></div>  Website Design</div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="col-lg-12">
+            <div className='dots'>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r8">
+          <div className="col-lg-12 jk1">
+            TEAM
+          </div>
+          <div className="col-lg-12 jk2">
+            Team Members
+          </div>
+          <div className="col-lg-12 jk3">
+            Contrary to popular belief. lorem ipsum is not a simply random text.It has
+            roots in a piece of classical latin literature from 45 BC, making it over
+            2000 years old
+          </div>
+          <div className="col-lg-4 ">
+
+          </div>
+          <div className="col-lg-4"></div>
+          <div className="col-lg-4"></div>
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
